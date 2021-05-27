bcdd6b4b3fe6e24b5b19fe63ec6d3b5a402fd4e3
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-05-27 10:03:19 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+/* // Object Task
+let person = {
+  name: "Brent",
+  age: 19,
+};
+
+console.log(person);
+
+// Dot Notation
+person.name = "Johnson";
+console.log(person.name);
+
+// Bracket Notation
+person["age"] = 20;
+console.log(person["age"]);
+
+// Array Task
+let selectedColors = ["red", "blue"];
+console.log(selectedColors);
+
+console.log(selectedColors[0]);
+
+// Lengths are dynamic
+selectedColors[2] = "green";
+console.log(selectedColors);
+
+// Properties using the dot(.)
+console.log(selectedColors.length); */
+
+// Exercise
+let person1 = {
+  name: "Brent Lee",
+  age: 19,
+};
+
+let person2 = {
+  name: "Johnson",
+  age: 20,
+};
+
+console.log(person1);
+console.log(person2);
+
+let personArray = [person1, person2];
+console.log(personArray);
